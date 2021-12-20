670ddd190cb4d7d8401de8123539484e4d33dcca
Felipe Nunes <fnap@cin.ufpe.br>
2021-12-19 21:22:47 -0300

    resolve conflicts

--- server/src/email/email.ts
+++ server/src/email/email.ts
@@ -1,7 +1,6 @@
 import * as nodemailer from "nodemailer";
 
 class Config {
-<<<<<<< HEAD
     HOST = "smtp.gmail.com";
     PORT = 587;
     USER = "felipe.nunesalmeida@gmail.com";
@@ -40,50 +39,3 @@ export class Mail {
 
 
 }
-=======
-	HOST = "";
-	PORT = 587;
-	USER = "";
-	PASSWORD = "";
-}
-
-export class Mail {
-	constructor(
-		public to?: string,
-		public subject?: string,
-		public message?: string
-	) {}
-
-	sendMail() {
-		let configs = new Config();
-
-		let mailOptions = {
-			from: "portalband@band.com.br",
-			to: this.to,
-			subject: this.subject,
-			html: this.message,
-		};
-
-		const transporter = nodemailer.createTransport({
-			host: configs.HOST,
-			port: configs.PORT,
-			secure: false,
-			auth: {
-				user: configs.USER,
-				pass: configs.PASSWORD,
-			},
-			tls: { rejectUnauthorized: false },
-		});
-
-		console.log(mailOptions);
-
-		transporter.sendMail(mailOptions, function (error: any, info: any) {
-			if (error) {
-				return error;
-			} else {
-				return "E-mail enviado com sucesso!";
-			}
-		});
-	}
-}
->>>>>>> 894413c60859d4293c527973b5bbfd8085d0a6e7
